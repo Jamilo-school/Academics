d4cff67768944b2f3509d3ec1dee3af39fda74f6
Jamilo-school <geofreyonyango167@gmail.com>
2023-08-10 15:06:10 +0300

    #lks

--- app.js
+++ app.js
@@ -50,3 +50,41 @@ function events() {
 
 events();
 
+////////////////////////////
+
+
+// Set the time limit for inactivity (in milliseconds)
+const inactivityTimeLimit = 5 * 60 * 1000; // 5 minutes
+
+const logoutUrl = "https://www.google.com/"; // Redirect to Google homepage
+
+let timeoutId;
+let popup;
+
+// Function to log out the user and show popup
+function logoutAndShowPopup() {
+  // Show popup message
+  popup = window.open("", "popup", "width=300,height=200");
+  popup.document.write(
+    "<p>We are logging you out due to page inactivity.</p>"
+  );
+
+  // Replace this with your actual logout logic
+  window.location.href = logoutUrl;
+}
+
+// Function to reset the inactivity timer
+function resetInactivityTimer() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(logoutAndShowPopup, inactivityTimeLimit);
+}
+
+// Add event listeners to track user activity
+document.addEventListener("mousemove", resetInactivityTimer);
+document.addEventListener("keydown", resetInactivityTimer);
+document.addEventListener("click", resetInactivityTimer);
+
+// Initialize the inactivity timer on page load
+resetInactivityTimer();
+
+
